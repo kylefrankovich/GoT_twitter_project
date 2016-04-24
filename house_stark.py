f1d264577daa3ff7ee924257d58a0dc5ba0f2f70
Kyle Frankovich <kylefrankovich@gmail.com>
2016-04-24 14:45:55 -0700

    updated crontab notes

--- house_stark.py
+++ house_stark.py
@@ -9,10 +9,11 @@ from pprint import pprint
 import json
 from datetime import datetime
 from pytz import timezone
+import os
 
 data_path = '/Users/kfranko/Box Sync/GoT_data/data'
 
-fileName = 'twitter_stream_test_NYPrimary.txt'
+fileName = 'earthday_2016_04_23-17_36_00.txt'
 
 fName = os.path.join(data_path, fileName)
 
@@ -20,7 +21,7 @@ fName = os.path.join(data_path, fileName)
 data = []
 with open(fName) as f:
     for line in f:
-        data.append(json.loads(line))
+        data.append(json.loads(line)) # makes a list of dictionaries
 
 len(data)
 
@@ -37,9 +38,29 @@ data[0]['user']['name']
 
 data[0].keys()
 
+tweet_text = data[0:5]['text']
+
 # for analysis, maybe we should use scikit learn's CountVectorizer?; can we have that search for a specific vocabulary
 # (ex: only count the number of times a character name appears)?; although perhaps we want to manually do this to
 # maintain the time information associated with the character name (ex: plotting the timeseries for the hour of the
 # original broadcast; X person is killed, and we see an explosion of tweets regarding their death)
 
 
+for tweet in data:
+    # now tweet is a dictionary
+    for attribute, value in tweet.iteritems():
+        print attribute, value # example usage
+
+
+# let's start working on a function to read in the tweet data and extract the useful
+# information: namely, the tweet text (data[X]['text']) and the tweet time (data[X]['created_at']):
+
+tweet_text = []
+def extract_text(input_data):
+    print len(input_data)-1
+    for e in range(0,(len(input_data)-1)):
+        tweet_text[e] = input_data[e]['text']
+    return tweet_text
+
+
+extract_text(data)
