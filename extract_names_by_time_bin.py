d6650198c7cede2798bcda4d06adc407faf9d998
Emily <emilyhalket@Emilys-MacBook-Air.local>
2016-04-29 16:06:31 -0700

    add placeholder for time bin script

---
+++ extract_names_by_time_bin.py
@@ -0,0 +1,16 @@
+'''
+
+placeholder for a function that will need to read in abbreviated file containing
+1) text of tweet
+2) time stamp of tweet
+
+given this data, we want to be able to
+
+- pass in desired character list (to be determined based on episode events)
+- time bin interval (potentially tbd based on episode or perhaps keep constant)
+
+desired output
+- character mentions per time bin for characters of interest
+
+
+'''
