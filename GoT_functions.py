6e48a7258745cb12596f80d33806ba4685b10adc
Emily <emilyhalket@Emilys-MacBook-Air.local>
2016-05-03 10:29:31 -0700

    changing a few style elements

--- GoT_functions.py
+++ GoT_functions.py
@@ -5,7 +5,7 @@ import json
 
 def tweet_loader(input_file):
     # here we add our basic method for loading tweets from the list of json objects
-    tweets = [] # create storage variable
+    tweets = []  # create storage variable
     tweets_file = open(input_file, "r")
     for line in tweets_file:
         try:
@@ -24,11 +24,13 @@ def tweet_loader(input_file):
 
 def tweet_reducer(input_file, episode_number, search_type, user_name, data_collector):
     output_file_name = '/Users/{}/Box Sync/GoT_data/data/episode_{}/ep_{}_{}_{}.txt'.format(user_name,episode_number,episode_number,data_collector,search_type)
-    raw_data = tweet_loader(input_file) # use tweet_loader function to load in raw data
+    raw_data = tweet_loader(input_file)  # use tweet_loader function to load in raw data
     reduced_tweets = []
     # create a dictionary of two items that we'll want (ex: tweet text and tweet time):
     for tweet in raw_data:
-        reduced_tweets.append({'text':tweet['text'], 'created_at':tweet['created_at']})
+        reduced_tweets.append({'text': tweet['text'], 'created_at': tweet['created_at']})
     # export reduced data to a text file:
-    json.dump(reduced_tweets, open(output_file_name,'w'))
+    json.dump(reduced_tweets, open(output_file_name, 'w'))
     print 'tweets reduced, yo'
+
+
