af6017168e822639b254b9711165718b38b835b8
Kyle Frankovich <kylefrankovich@gmail.com>
2016-05-21 23:08:14 -0700

    new plots/updates

--- data_viz/GoT_search_plotting_script.R
+++ data_viz/GoT_search_plotting_script.R
@@ -23,7 +23,8 @@ df_filtered = filter(df, names %in% character_threshold$names)
 episodes_1_4_plot<-ggplot(df_filtered,
                               aes(x=factor(names),y=count_percent,
                                   fill=factor(episode)), color=factor(episode)) +
-  labs(x = 'names', y = 'percentage of character mentions', title = 'Character Mentions by Episode') +
+  labs(x = 'names', y = 'percentage of character mentions', 
+       title = 'Character Mentions by Episode') +
   stat_summary(fun.y=mean,position=position_dodge(),geom="bar")
 
 ggplotly(episodes_1_4_plot)
@@ -35,6 +36,7 @@ ggplotly(episodes_1_4_plot)
 brienne_tormund = c('brienne', 'tormund')
 df_brienne_tormund = filter(df, names %in% brienne_tormund)
 
+brienne_tormund_ep_descriptions = c('Brienne saves Sansa')
 
 brienne_tormund_line_plot = ggplot(data=df_brienne_tormund, 
                                    aes(x=episode, y=count_percent, 
@@ -47,11 +49,31 @@ brienne_tormund_line_plot = ggplot(data=df_brienne_tormund,
 
 ggplotly(brienne_tormund_line_plot)
 
-plotly_POST(brienne_tormund_line_plot, filename = "brienne_tormund_plot", world_readable=TRUE)
+plotly_POST(brienne_tormund_line_plot, 
+            filename = "brienne_tormund_plot", world_readable=TRUE)
 
-# embedded link example: https://plot.ly/~Dreamshot/411.embed?width=650&modebar=false&link=false
+# embedded link example: 
+# https://plot.ly/~Dreamshot/411.embed?width=650&modebar=false&link=false
 # adjusts horizontal size, as well as removes edit link + plotly toolbar
 
 
+# line plot of selected characters of interest:
+
+# Brienne and Tormund line plot:
+
+character_threshold <- filter(df, count_percent > 5)
+df_filtered_characters_of_interest = filter(df, names %in% character_threshold$names)
+
+# add notes for each character/important events?
+characters_of_interest_line_plot = ggplot(data=df_filtered_characters_of_interest, 
+                                   aes(x=episode, y=count_percent, 
+                                       group=names, colour=names,
+                                       text = df_filtered_characters_of_interest$X)) +
+  geom_line() +
+  labs(x = 'episode', y = 'percentage of mentions', 
+       title = "characters of interest plot", colour = 'characters')+
+  geom_point(size = 2)
+
+ggplotly(characters_of_interest_line_plot)
 
 
