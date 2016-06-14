fdef36f3e79516ac7f9160978c09d2ed2dd07d6a
Kyle Frankovich <kylefrankovich@gmail.com>
2016-06-14 12:42:22 -0700

    processed ep 7 search, updated search script for ep 8

--- kyle_data_processing_analysis.py
+++ kyle_data_processing_analysis.py
@@ -2,16 +2,16 @@ __author__ = 'kfranko'
 
 import os
 
-data_path = '/Users/kfranko/Box Sync/GoT_data/data/episode_6'
+data_path = '/Users/kfranko/Box Sync/GoT_data/data/episode_7'
 
-fileName = 'GoT_search_ep6_48_hrs_2016_06_03-00_34_26.txt'
+fileName = 'GoT_search_ep7_48_hrs_2016_06_09-15_50_29.txt'
 
 fName = os.path.join(data_path, fileName)
 
 from GoT_functions import tweet_loader, tweet_reducer
 
 
-tweet_reducer(fName, '6', 'search_48_hrs', 'kfranko', 'kyle')
+tweet_reducer(fName, '7', 'search_48_hrs', 'kfranko', 'kyle')
 
 
 episode_1_search_tweets = tweet_loader(fName)
@@ -23,7 +23,7 @@ len(episode_1_search_tweets[0])
 
 from GoT_functions import preprocess_character_counts
 
-ep_num = '6'
+ep_num = '7'
 user_name = 'kfranko'
 search_type = 'search_48_hrs'
 data_collector = 'kyle'
