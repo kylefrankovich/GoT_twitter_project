97e1aadbfff5a4abf05ad6158726af8d050b31b2
Kyle Frankovich <kylefrankovich@gmail.com>
2016-06-30 13:45:45 -0700

    reduced/name counts for eps 8/9/10
    still need to update the character names library, and get updated name
    counts for all 10 eps for final dataset

--- kyle_data_processing_analysis.py
+++ kyle_data_processing_analysis.py
@@ -2,16 +2,16 @@ __author__ = 'kfranko'
 
 import os
 
-data_path = '/Users/kfranko/Box Sync/GoT_data/data/episode_9'
+data_path = '/Users/kfranko/Box Sync/GoT_data/data/episode_10'
 
-fileName = 'GoT_search_ep9_48_hrs_2016_06_22-17_00_05.txt'
+fileName = 'GoT_search_ep10_48_hrs_2016_06_29-12_16_25.txt'
 
 fName = os.path.join(data_path, fileName)
 
 from GoT_functions import tweet_loader, tweet_reducer
 
 
-tweet_reducer(fName, '9', 'search_48_hrs', 'kfranko', 'kyle')
+tweet_reducer(fName, '10', 'search_48_hrs', 'kfranko', 'kyle')
 
 
 episode_1_search_tweets = tweet_loader(fName)
@@ -23,7 +23,7 @@ len(episode_1_search_tweets[0])
 
 from GoT_functions import preprocess_character_counts
 
-ep_num = '7'
+ep_num = '10'
 user_name = 'kfranko'
 search_type = 'search_48_hrs'
 data_collector = 'kyle'
